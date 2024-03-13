10b71e1ffea732fdb957c3d867da5846ee33da5d
Balázs Varga <balazs.varga@apter.tech>
2024-03-13 21:34:23 +0100

    feat(sandbox): Add a custom android sandbox

---
+++ robolectric-extension/src/main/kotlin/tech/apter/junit/jupiter/robolectric/internal/AndroidMainThreadExecutor.kt
@@ -0,0 +1,33 @@
+package tech.apter.junit.jupiter.robolectric.internal
+
+import java.util.concurrent.Callable
+import java.util.concurrent.ExecutionException
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import java.util.concurrent.ThreadFactory
+import org.robolectric.util.Util
+
+internal object AndroidMainThreadExecutor {
+    private val executorService: ExecutorService = Executors.newSingleThreadExecutor(mainThreadFactory())
+
+    fun <T : Any?> execute(callable: Callable<T>): T {
+        val future = executorService.submit(callable)
+        try {
+            return future.get()
+        } catch (e: InterruptedException) {
+            future.cancel(true)
+            throw RuntimeException(e)
+        } catch (e: ExecutionException) {
+            throw Util.sneakyThrow<RuntimeException>(e.cause)
+        }
+    }
+
+    fun shutdown() = executorService.shutdown()
+
+    private fun mainThreadFactory(): ThreadFactory {
+        return ThreadFactory { r: Runnable? ->
+            val name = "Android SDK"
+            Thread(ThreadGroup(name), r, "$name Main Thread")
+        }
+    }
+}
