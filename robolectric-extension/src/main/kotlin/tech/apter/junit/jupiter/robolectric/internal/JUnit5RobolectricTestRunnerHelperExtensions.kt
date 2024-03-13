92ff2ce14003584664389e0ae30d22402aa92972
Balázs Varga <balazs.varga@apter.tech>
2024-03-13 22:35:57 +0100

    feat(runner): Add Robolectric test runner helper

--- robolectric-extension/src/main/kotlin/tech/apter/junit/jupiter/robolectric/internal/JUnit5RobolectricTestRunnerHelperExtensions.kt
+++ robolectric-extension/src/main/kotlin/tech/apter/junit/jupiter/robolectric/internal/JUnit5RobolectricTestRunnerHelperExtensions.kt
@@ -2,19 +2,19 @@ package tech.apter.junit.jupiter.robolectric.internal
 
 import java.util.concurrent.Callable
 
-internal inline fun <T> JUnit5RobolectricTestRunnerHelper.runOnMainThread(crossinline action: JUnit5RobolectricTestRunnerHelper.() -> T): T =
+internal fun <T> JUnit5RobolectricTestRunnerHelper.runOnMainThread(action: JUnit5RobolectricTestRunnerHelper.() -> T): T =
     sdkEnvironment.runOnMainThread(
         Callable {
             action()
         }
     )
 
-internal inline fun <T> JUnit5RobolectricTestRunnerHelper.runOnMainThreadWithRobolectric(crossinline action: JUnit5RobolectricTestRunnerHelper.() -> T): T =
+internal fun <T> JUnit5RobolectricTestRunnerHelper.runOnMainThreadWithRobolectric(action: JUnit5RobolectricTestRunnerHelper.() -> T): T =
     runOnMainThread {
         runWithRobolectric(action)
     }
 
-internal inline fun <T> JUnit5RobolectricTestRunnerHelper.runWithRobolectric(crossinline action: JUnit5RobolectricTestRunnerHelper.() -> T): T {
+internal fun <T> JUnit5RobolectricTestRunnerHelper.runWithRobolectric(action: JUnit5RobolectricTestRunnerHelper.() -> T): T {
     loadRobolectricClassLoader()
     return action().also {
         resetClassLoaderToOriginal()
