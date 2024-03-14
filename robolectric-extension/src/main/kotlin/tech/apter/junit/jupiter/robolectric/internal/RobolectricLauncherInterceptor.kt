7a10ff705897445b469b90b01781afe4aca551ac
Balázs Varga <balazs.varga@apter.tech>
2024-03-14 07:24:11 +0100

    feat(junit5): Add a Launcher interceptor

---
+++ robolectric-extension/src/main/kotlin/tech/apter/junit/jupiter/robolectric/internal/RobolectricLauncherInterceptor.kt
@@ -0,0 +1,37 @@
+package tech.apter.junit.jupiter.robolectric.internal
+
+import java.util.concurrent.atomic.AtomicBoolean
+import org.junit.platform.launcher.LauncherInterceptor
+import tech.apter.junit.jupiter.robolectric.internal.placeholder.RobolectricPlaceholderTest
+
+class RobolectricLauncherInterceptor : LauncherInterceptor {
+    private inline val logger get() = createLogger()
+    private val testRunnerHelper by lazy { JUnit5RobolectricTestRunnerHelper() }
+
+    override fun <T : Any?> intercept(invocation: LauncherInterceptor.Invocation<T>): T {
+        logger.trace { "intercept" }
+
+        if (!robolectricLoaded.getAndSet(true)) {
+            // Create a dummy environment before JUnit5 Jupiter Engine creates any test instance.
+            // The test class should be loaded by the org.robolectric.internal.AndroidSandbox.SdkSandboxClassLoader.
+            // Otherwise, the InstrumentationRegistry.getInstrumentation() would be null.
+            // This environment should be replaced before starting any test.
+            testRunnerHelper.createTestEnvironmentForClass(RobolectricPlaceholderTest::class.java)
+            testRunnerHelper.loadRobolectricClassLoader()
+            testRunnerHelper.clearCachedRobolectricTestRunnerEnvironment()
+        }
+
+        return invocation.proceed()
+    }
+
+    override fun close() {
+        logger.trace { "close" }
+        testRunnerHelper.resetClassLoaderToOriginal()
+        AndroidMainThreadExecutor.shutdown()
+    }
+
+    companion object {
+        @JvmStatic
+        private val robolectricLoaded = AtomicBoolean(false)
+    }
+}
---
+++ robolectric-extension/src/main/kotlin/tech/apter/junit/jupiter/robolectric/internal/placeholder/RobolectricPlaceholderTest.kt
@@ -0,0 +1,8 @@
+package tech.apter.junit.jupiter.robolectric.internal.placeholder
+
+import org.junit.jupiter.api.Test
+
+internal class RobolectricPlaceholderTest {
+    @Test
+    fun placeholderTestMethod() = Unit
+}
