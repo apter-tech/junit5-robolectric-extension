bdd3e5b7a76d495618d0eb9e13b77b9aca5c6b31
Balázs Varga <balazs.varga@apter.tech>
2024-05-02 13:14:13 +0200

    ci: Publish plugin to the gradle plugin portal after tests finished successfully (#41)

--- publishGradlePlugin.sh
+++ publishGradlePlugin.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 function publishGradlePlugin() {
-  ./gradlew :robolectric-extension-gradle-plugin:publishToMavenLocal --configure-on-demand
   local -r robolectricGradlePluginVersion=$(cat < gradle.properties | grep tech.apter.junit5.robolectric.extension.version | cut -d'=' -f2)
 
   if [[ "$robolectricGradlePluginVersion" != *SNAPSHOT ]]; then
@@ -9,6 +8,8 @@ function publishGradlePlugin() {
       --configure-on-demand \
       -D"gradle.publish.key=${JUNIT5_ROBOLECTRIC_EXTENSION_GRADLE_PLUGIN_PORTAL_KEY}" \
       -D"gradle.publish.secret=${JUNIT5_ROBOLECTRIC_EXTENSION_GRADLE_PLUGIN_PORTAL_SECRET}"
+  else
+    echo -e "publishGradlePlugin skipped. Run the following command if you want to publish the plugin locally:\n\t./gradlew :robolectric-extension-gradle-plugin:publishToMavenLocal --configure-on-demand\n"
   fi
 }
 
